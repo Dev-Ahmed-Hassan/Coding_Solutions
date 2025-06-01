17cc4d9064e9100e4ceadd328a9d1ba5836543c3
Ahmed Hassan <ahmed.cs.uet@gmail.com>
2025-06-02 01:06:34 +0500

    Update 1-Two_Sum.cpp

---
+++ Blind 75/Easy/1-Two_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int length = nums.size();
+        for(int i=0; i<length-1; i++)
+        {
+            for(int j=i+1; j<length; j++)
+            {
+                if( nums[i] + nums[j] == target )
+                {
+                    vector<int> ans= {i, j};
+                    return ans;
+                }
+            }
+        }
+        return {};
+    }
+};
