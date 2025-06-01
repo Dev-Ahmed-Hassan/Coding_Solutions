05a3d4b2a1f510a39e61479566482324538c72b8
Ahmed Hassan <ahmed.cs.uet@gmail.com>
2025-06-01 17:52:18 +0500

    Longest Consecutive Sequence

---
+++ Blind 75/Medium/128-Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) 
+    {
+        // a longest and a current variable for streaks
+        int longest = 1;
+        int current = 1;
+
+        // for empty input vectors
+        if(nums.size() == 0)
+        {
+            longest = 0;
+            current = 0;
+        }
+
+        // size calculation
+        int size = nums.size()-1;
+    
+        // sorting the input vector
+        sort(nums.begin(), nums.end());
+
+        // main for loop
+        for(int i=0; i<size; i++)
+        {
+            // check if the next number in array is the current number + 1
+            if(nums[i]+1 == nums[i+1])
+            {
+                current+=1;
+            }
+            // do nothing if the number is equal to the previous number
+            else if (nums[i] == nums[i+1])
+            {
+                
+            }
+            // if the streak is broken
+            else
+            {
+                // if the streak is longer than the previous
+                if(current > longest)
+                {
+                    longest = current;
+                }
+                current = 1;
+            }
+        }
+        //  if the streak is never broken (for 0,5,6,7) now the longest is never updated, so to cover that, here is the condition.
+        if(current > longest)
+        longest = current;
+    
+        // return the answer;
+        return longest;
+    }
+};
