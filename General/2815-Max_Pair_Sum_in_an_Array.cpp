bbfd573d1551724888a43eedfe23747c0dffdada
Ahmed Hassan <ahmed.cs.uet@gmail.com>
2025-06-06 23:29:50 +0500

    damn

--- General/2815-Max_Pair_Sum_in_an_Array.cpp
+++ General/2815-Max_Pair_Sum_in_an_Array.cpp
@@ -31,3 +31,33 @@ public:
         return maxDigit;
     }
 };
+
+
+// Damn.... this shit is cool
+// He made two arrays [0,1,2,3,4,5,6,7,8,9] these are indexes not values...
+// then placed the values in the respective index referring to the maximum digit...
+// then he added them.... damn
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int max1[10] = {0}, max2[10] = {0};
+        for (int num : nums) {
+            int d = 0, t = num;
+            while (t) {
+                d = max(d, t % 10);
+                t /= 10;
+            }
+            if (num > max1[d]) {
+                max2[d] = max1[d];
+                max1[d] = num;
+            } 
+            else if (num > max2[d]) max2[d] = num;
+        }
+        int res = -1;
+        for (int i = 1; i < 10; ++i) {
+            if (max2[i]) res = max(res, max1[i] + max2[i]);
+        }
+        return res;
+    }
+};
