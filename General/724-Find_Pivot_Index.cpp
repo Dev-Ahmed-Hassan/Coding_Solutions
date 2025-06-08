d4e88127ac1f536a8281560d1652a41c129a453d
Ahmed Hassan <ahmed.cs.uet@gmail.com>
2025-06-08 16:10:42 +0500

    Easy... they say 😭😭

---
+++ General/724-Find_Pivot_Index.cpp
@@ -0,0 +1,37 @@
+// Calculated the total sum
+// For each number in nums, we checked the condition 😭😭
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) 
+    {
+        int total=0;
+        int i=0;
+        int j=0;
+        int sum = 0;
+        
+        
+        for(int x: nums)
+        {
+            sum+=x;
+        }
+
+        
+        
+        int ptr1=0, ptr2 =0, hsum = 0;
+
+        
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(i!= 0)
+            {
+                hsum+= nums[i-1];
+            }
+            if(hsum == (sum-nums[i])/2.0f)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
--- Test.cpp
+++ Test.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 
 
 
 int main()
 {
-    string arr[5] = {"1","2","3"};
+    vector<int> nums = {1,7,3,6,5,6};
+    
+        int total=0;
+        int i=0;
+        int j=0;
+        int sum = 0;
+        vector<int> arr;
+        
+        for(int x: nums)
+        {
+            sum+=x;
+        }
 
-    for(int i =0; i<5; i++)
-    {
-        cout << arr[i] << endl;
-    }
+        for(int x: nums)
+        {
+            arr.push_back(sum - x);
+        }
+        
+        int ptr1=0, ptr2 =0, hsum = 0;
+
+        
+
+        while(ptr1 < arr.size() )
+        {
+            if(ptr2 < ptr1)
+            {
+                hsum+=nums[ptr2];
+                ptr2++;
+            }
+            else
+            {
+                if(arr[ptr1]/2 == hsum)
+                {
+                    return ptr1;
+                }
+                else
+                {
+                    ptr1++;
+                }
+            }
+        }
+    
 }
