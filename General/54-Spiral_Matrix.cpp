6f119db42fc2bfc2b678fc622f2c5003b7902732
Ahmed Hassan <ahmed.cs.uet@gmail.com>
2025-08-02 19:17:30 +0500

    54 leetcode

---
+++ General/54-Spiral_Matrix.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool function(vector<vector<int>>& matrix, vector<int>& arr, int row,
+                  int col, int it) {
+                   int count = col*row;
+        
+
+        int cR = 0, cC = 0;
+        for (int j = it; j < col - it; j++) {
+            arr.push_back(matrix[it][j]);
+            cC = j;
+        }
+        if(arr.size() >= count) return false;
+        
+        for (int i = it+1; i < row - it; i++) {
+            arr.push_back(matrix[i][cC]);
+            cR = i;
+        }
+
+        if(arr.size() >= count) return false;
+        
+        for (int j = cC-1; j >= it; j--) {
+            arr.push_back(matrix[cR][j]);
+            cC = j;
+        }
+        
+        if(arr.size() >= count) return false;
+
+        for (int i = cR-1; i > it; i--) {
+            arr.push_back(matrix[i][cC]);
+        }
+
+        if(arr.size() >= count) return false;
+
+        return true;
+    }
+
+    vector<int> spiralOrder(vector<vector<int>>& matrix) { 
+        vector<int> result; 
+        int it=0;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        
+
+        while (function(matrix, result, row, col, it))
+        {
+            it++;
+        }
+        return result;
+    }
+};
